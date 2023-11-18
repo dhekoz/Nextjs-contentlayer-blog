4fdaad01c84ab32cda2acbf7f1c3397b98fd250f
dhekoz <petrus.d.r.soge@gmail.com>
2023-11-19 01:06:16 +0700

    footers

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,8 @@
-"use client"
+"use client";
 import React from "react";
 import { useForm } from "react-hook-form";
+import { DribbbleIcon, GithubIcon, LinkedinIcon, TwitterIcon } from "../Icons";
+import Link from "next/link";
 
 const Footer = () => {
   const {
@@ -12,21 +14,51 @@ const Footer = () => {
   console.log(errors);
   return (
     <footer className="mt-16 rounded-2xl bg-dark m-10 flex flex-col items-center text-light">
-      <h3 className="mt-16 font-medium text-center capitalize text-3xl px-4">Interesting Stories | Updates | Guides</h3>
+      <h3 className="mt-16 font-medium text-center capitalize text-3xl px-4">
+        Interesting Stories | Updates | Guides
+      </h3>
       <p className="mt-5 px-4 text-center w-3/5 font-light text-base">
-        Subscribe to learn about new technology and updates. Join over 5000+
-        members community to stay up to date with latest news.
+        Subscribe to learn about new technology and updates. <br />
+        Join over 5000+ members community to stay up to date with latest news.
       </p>
-      <form onSubmit={handleSubmit(onSubmit)} className="mt-6 min-w-[384px] flex items-stretch bg-light p-2 rounded mx04">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="mt-6 min-w-[384px] flex items-stretch bg-light p-2 rounded mx-0"
+      >
         <input
           type="text"
           placeholder="Enter your email"
           {...register("Email", { required: true })}
-          className="w-full bg-transparent text-dark focus:border-dark focus: ring-0 border-0 border-b mr-2 pb-1"
+          className="w-full bg-transparent pl-0 text-dark focus:border-dark focus: ring-0 border-0 border-b mr-2 pb-1"
         />
 
-        <input type="submit" className="bg-dark text-light cursor-pointer font-medium rounded px-5 py-1" />
+        <input
+          type="submit"
+          className="bg-dark text-light cursor-pointer font-medium rounded px-5 py-1"
+        />
       </form>
+      <div className="flex items-center mt-8">
+        <a href="http://example.com" className="inline-block w-6 h-6 mr-4">
+          <LinkedinIcon className="hover:scale-125 transition-all ease duration-200" />
+        </a>
+        <a href="http://example.com" className="inline-block w-6 h-6 mr-4">
+          <TwitterIcon className="hover:scale-125 transition-all ease duration-200" />
+        </a>
+        <a href="http://example.com" className="inline-block w-6 h-6 mr-4">
+          <GithubIcon className="hover:scale-125 transition-all ease duration-200 fill-light" />
+        </a>
+        <a href="http://example.com" className="inline-block w-6 h-6 mr-4">
+          <DribbbleIcon className="hover:scale-125 transition-all ease duration-200" />{" "}
+        </a>
+      </div>
+      <div className="w-full mt-20 relative font-medium border-t border-solid border-light py-6 px-8 flex flex-row items-center justify-between">
+        <span className="text-center">&copy;2023 CodeBucks. All rights reserved. </span>
+        <Link href="/sitemap.xml" className="text-center underline">sitemap.xml</Link>
+        <div className="text-center underline">
+          Made with &hearts; by{" "}
+          <a href="https://devdreaming.com">CodeBucks</a>
+        </div>
+      </div>
     </footer>
   );
 };
