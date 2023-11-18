9e58e7aad0e22dbaa7b54476e0c921297c3e26eb
dhekoz <petrus.d.r.soge@gmail.com>
2023-11-19 01:18:58 +0700

    footer revisi

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -13,8 +13,8 @@ const Footer = () => {
   const onSubmit = (data) => console.log(data);
   console.log(errors);
   return (
-    <footer className="mt-16 rounded-2xl bg-dark m-10 flex flex-col items-center text-light">
-      <h3 className="mt-16 font-medium text-center capitalize text-3xl px-4">
+    <footer className="mt-24 rounded-2xl bg-dark m-10 flex flex-col items-center text-light">
+      <h3 className="mt-10 font-medium text-center capitalize text-3xl px-4">
         Interesting Stories | Updates | Guides
       </h3>
       <p className="mt-5 px-4 text-center w-3/5 font-light text-base">
@@ -23,7 +23,7 @@ const Footer = () => {
       </p>
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="mt-6 min-w-[384px] flex items-stretch bg-light p-2 rounded mx-0"
+        className="mt-6 min-w-[384px] flex items-stretch bg-light p-2 rounded mx-04"
       >
         <input
           type="text"
@@ -51,12 +51,12 @@ const Footer = () => {
           <DribbbleIcon className="hover:scale-125 transition-all ease duration-200" />{" "}
         </a>
       </div>
-      <div className="w-full mt-20 relative font-medium border-t border-solid border-light py-6 px-8 flex flex-row items-center justify-between">
+      <div className="w-full mt-5 relative font-medium border-t border-solid border-light py-6 px-8 flex flex-row items-center justify-between">
         <span className="text-center">&copy;2023 CodeBucks. All rights reserved. </span>
         <Link href="/sitemap.xml" className="text-center underline">sitemap.xml</Link>
-        <div className="text-center underline">
+        <div className="text-center">
           Made with &hearts; by{" "}
-          <a href="https://devdreaming.com">CodeBucks</a>
+          <a href="https://devdreaming.com" className="underline">CodeBucks</a>
         </div>
       </div>
     </footer>
